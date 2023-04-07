3146facfa3770f3b6456c7d1e588582ad9c9f9e5
miracleonyenma <miracleficient@gmail.com>
2023-04-07 21:54:11 +0100

    updates: add phosphor icon, add socials link list

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,12 @@
 <script setup>
 import { useMotion } from "@vueuse/motion";
+import {
+  PhTwitterLogo,
+  PhLinkedinLogo,
+  PhGithubLogo,
+  PhDribbbleLogo,
+  PhReadCvLogo,
+} from "@phosphor-icons/vue";
 const specialGreeting = useSpecialGreeting();
 
 const heroContent = ref();
@@ -22,6 +29,36 @@ const heroContentMotionInstance = useMotion(heroContent, {
     },
   },
 });
+
+const socialLinks = [
+  {
+    name: "Twitter",
+    url: "https://twitter.com/miracleio",
+    icon: PhTwitterLogo,
+  },
+  {
+    name: "LinkedIn",
+    url: "https://linkedin.com/in/miracleio",
+    icon: PhLinkedinLogo,
+  },
+  {
+    name: "GitHub",
+    url: "https://github.com/miracleonyenma",
+    icon: PhGithubLogo,
+  },
+
+  {
+    name: "Dribbble",
+    url: "https://dribbble.com/miracleio",
+    icon: PhDribbbleLogo,
+  },
+
+  {
+    name: "My Resume",
+    url: "https://docs.google.com/document/d/1jqlXD4OjNMssz_IBJOe7NuKp8uQaYRs5_3XK-6_lf4Y/edit?usp=sharing",
+    icon: PhReadCvLogo,
+  },
+];
 </script>
 <template>
   <main class="site-main">
@@ -47,6 +84,19 @@ const heroContentMotionInstance = useMotion(heroContent, {
             I'm a Designer and Frontend Developer obsessed with crafting
             beautiful experiences ✨
           </p>
+          <ul class="socials-links group">
+            <!-- <NuxtLink to="" -->
+            <li
+              v-for="(link, index) of socialLinks"
+              :key="link.name"
+              class="link-item"
+              :style="{ transitionDuration: (index + 1) * 0.5 + 's' }"
+            >
+              <NuxtLink :to="link.url" :target="link.name" :title="link.name" class="social-link">
+                <component :is="link.icon" weight="fill" class="icon w-8 h-8" />
+              </NuxtLink>
+            </li>
+          </ul>
         </div>
       </div>
     </header>
@@ -66,11 +116,28 @@ const heroContentMotionInstance = useMotion(heroContent, {
 }
 
 .hero-img-cont > img {
-  @apply rounded-3xl;
+  @apply rounded-full;
   @apply ring-8 ring-blue-ryb dark:ring-han-purple ring-offset-8 ring-offset-space-cadet-50 dark:ring-offset-space-cadet-900;
 }
 
 .site-hero .hero-content {
-  @apply md:col-span-2 md:col-start-1 md:row-start-1 w-full max-w-7xl m-auto;
+  @apply md:col-span-2 md:col-start-1 md:row-start-1 flex flex-col gap-2 w-full max-w-7xl m-auto;
+}
+
+.socials-links {
+  @apply flex gap-2 w-fit p-2 border border-space-cadet-200 dark:border-space-cadet-800 rounded-full;
+}
+
+.socials-links .link-item {
+  @apply inline-block p-3 rounded-full;
+  @apply transition-all;
+}
+
+.socials-links:hover .link-item {
+  @apply bg-space-cadet-200 dark:bg-space-cadet-800;
+}
+
+.link-item:hover .icon {
+  @apply transition transform scale-110;
 }
 </style>
